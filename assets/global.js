c4f464db671b727ffcf33cdff2b2f3102dff4535
dev-whale <77664713+dev-whale@users.noreply.github.com>
2024-12-26 19:24:39 +0100

    DEV: Custom Accordion added.

--- assets/global.js
+++ assets/global.js
@@ -1267,3 +1267,100 @@ class BulkAdd extends HTMLElement {
 if (!customElements.get('bulk-add')) {
   customElements.define('bulk-add', BulkAdd);
 }
+
+class AccordionRow extends HTMLElement {
+  constructor() {
+    super();
+
+    this.details = this.querySelector('details');
+    this.summary = this.querySelector('summary');
+    this.content = this.querySelector('.accordion__content');
+
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+
+    if (this.summary) {
+      this.summary.addEventListener('click', (e) => this.onClick(e));
+    }
+  }
+
+  onClick(e) {
+    e.preventDefault();
+
+    this.details.style.overflow = 'hidden';
+    if (this.isClosing || !this.details.open) {
+      this.open();
+    } else if (this.isExpanding || this.details.open) {
+      this.shrink();
+    }
+  }
+
+  shrink() {
+    this.isClosing = true;
+
+    const summaryStyle = window.getComputedStyle(this.summary);
+    const startHeight = `${this.details.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + parseInt(summaryStyle.marginTop)}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.details.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: 250,
+        easing: 'ease',
+      }
+    );
+
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => (this.isClosing = false);
+  }
+
+  open() {
+    this.details.style.height = `${this.details.offsetHeight}px`;
+    this.details.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+
+    const summaryStyle = window.getComputedStyle(this.summary);
+    const startHeight = `${this.details.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + parseInt(summaryStyle.marginTop) + this.content.offsetHeight}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.details.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: 400,
+        easing: 'ease-out',
+      }
+    );
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => (this.isExpanding = false);
+  }
+
+  onAnimationFinish(open) {
+    this.details.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.details.style.height = this.details.style.overflow = '';
+  }
+}
+
+customElements.define('accordion-row', AccordionRow);
+
+const accordionRow = document.createElement('accordion-row');
+document.body.appendChild(accordionRow);
